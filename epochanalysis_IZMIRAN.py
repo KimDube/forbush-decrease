22be54034ba3bb1d9ab3307daa00d3d7eb59fbd2
Kimberlee <krd020@mail.usask.ca>
2017-09-18 11:02:16 -0600

    Basic analysis- works for OSIRIS aerosol versions 5.07 and 6.00. TODO: Add significance testing and OMPS functionality.

--- epochanalysis_IZMIRAN.py
+++ epochanalysis_IZMIRAN.py
@@ -23,28 +23,53 @@ import datetime
 import xarray
 
 
-# Number of days in each epoch. 14 days (2 weeks) before event to 35 days (5 weeks) after event.
-EPOCH_LENGTH = 50
+# -----------------------------------------------------------------------------
+def anomalize(timeseries):
+    """
+    Centre time series on zero (percent difference from mean). Then take anomaly
+    as difference from 35 day running mean.
+    :param timeseries: 1d array in steps of 1 day
+    :return: timeseries as percent difference from mean, filtered with 35 day
+    high-pass.
+    """
+    # First centre data on zero: fig 2a of Laken and Calogovic 2013
+    timeseries = 100 * (timeseries - np.nanmean(timeseries)) / np.nanmean(timeseries)
 
-# MagM >= 3%
-EVENTS = ['2002-02-04', '2002-03-18', '2002-04-17', '2002-05-23', '2002-08-18', '2003-01-26', '2003-05-29',
-          '2003-08-17', '2003-10-30', '2003-11-30', '2004-07-26', '2004-09-13', '2004-11-09', '2005-01-18',
-          '2005-05-15', '2005-07-17', '2005-09-11', '2006-07-09', '2006-12-14', '2007-01-29', '2010-08-03',
-          '2011-04-11', '2011-06-22', '2011-08-05', '2011-10-30', '2012-01-22', '2012-03-08', '2012-04-04',
-          '2012-07-14', '2012-09-04', '2012-10-11', '2012-11-23', '2013-01-16', '2013-03-17', '2013-05-31',
-          '2013-07-12', '2014-04-18', '2014-06-17', '2014-09-12', '2014-12-21', '2015-03-17', '2015-05-06',
-          '2015-06-22', '2015-09-07', '2015-11-06']
-
-NUM_EVENTS = len(EVENTS)
+    # Take anomaly in counts to be difference from 35 day running mean
+    #  -> high-pass filter.
+    y = pd.Series(timeseries)
+    f2 = y.rolling(center=True, window=35).mean()
+    u = y - f2
+    timeseries = np.array(u)
+    return timeseries
 
 
 # -----------------------------------------------------------------------------
-def findmeangcr(start_days, contour_plot=0):
+def shift_epoch(event_array, initial_epoch_start):
+    """
+    Shifts first day in epoch so that the minima will be aligned with day zero.
+    (Epoch start should be 2 weeks before minima, hence the -14 shift factor).
+    :param event_array:
+    :param initial_epoch_start:
+    :return: list of new epoch starts as strings.
+    """
+    new_onset = []
+    for j in range(NUM_EVENTS):
+        d = np.argmin(event_array[:, j])
+        ne = datetime.datetime.strptime(initial_epoch_start[j], "%Y-%m-%d") + datetime.timedelta(int(d) - 14)
+        new_onset.append(ne.strftime('%Y-%m-%d'))
+
+    return new_onset
+
+
+# -----------------------------------------------------------------------------
+def find_mean_gcr(epoch_starts, plot=0, shifted=0):
     """
     Epoch analysis of GCR from Moscow neutron monitor.
     Epoch start dates are shifted so maximum decrease is at day zero.
-    :param start_days:
-    :param contour_plot:
+    :param epoch_starts:
+    :param plot:
+    :param shifted:
     :return:
     """
     open_file = open('/home/kimberlee/Masters/ForbushDecrease/MOSC_2002_2017.txt', "r")
@@ -58,44 +83,58 @@ def findmeangcr(start_days, contour_plot=0):
     counts = np.array(counts)
     counts[counts < 1] = np.nan
 
-    # First centre data on zero: fig 2a of Laken and Calogovic 2013
-    counts = 100 * (counts - np.nanmean(counts)) / np.nanmean(counts)
-
-    # Take anomaly in counts to be difference from 35 day running mean -> high-pass filter.
-    y = pd.Series(counts)
-    f2 = y.rolling(center=True, window=35).mean()
-    u = y - f2
-    counts = np.array(u)
+    counts = anomalize(counts)  # find anomaly
 
     # convert strings to match start_days
     for j in range(len(dates)):
         dates[j] = dates[j].replace('.', '-')
     dates = np.array(dates)
-    start_days = np.array(start_days)
+    epoch_starts = np.array(epoch_starts)
 
     # search for epoch start_days and store corresponding count values for complete epoch
     gcrarr = np.zeros((EPOCH_LENGTH, NUM_EVENTS))
-    new_start_days = np.zeros(NUM_EVENTS)
-    for j in range(NUM_EVENTS):
-        temp = np.where(dates == start_days[j])
-        temp = int(temp[0][0])
-        for k in range(EPOCH_LENGTH):
-            gcrarr[k, j] = counts[temp+k]
 
-        # shift so minimum is at zero and find new epoch.
-        d = np.argmin(gcrarr[:, j])
-        start_days[j] = datetime.datetime.strptime(start_days[j], "%Y-%m-%d") + datetime.timedelta(int(d)-14)
-        new_start_days = start_days[j]
-        temp = np.where(dates == start_days[j])
-        temp = int(temp[0][0])
-        for k in range(EPOCH_LENGTH):
-            gcrarr[k, j] = counts[temp+k]
+    # If shifted is 0 then epoch starts need to be shifted so minima is aligned
+    #  with day zero (default).
+    if shifted == 0:
+        new_start_days = []
+        for j in range(NUM_EVENTS):
+            temp = np.where(dates == epoch_starts[j])
+            temp = int(temp[0][0])
+            for k in range(EPOCH_LENGTH):
+                gcrarr[k, j] = counts[temp+k]
+            # shift the event onset times so the minima is aligned with day zero.
+            new_start_days = shift_epoch(gcrarr, epoch_starts)
+        # Find new epochs
+        for j in range(NUM_EVENTS):
+            temp = np.where(dates == new_start_days[j])
+            temp = int(temp[0][0])
+            for k in range(EPOCH_LENGTH):
+                gcrarr[k, j] = counts[temp+k]
+        # Save so this doesn't always have to be done
+        np.save('./shifted_epoch_onsets', new_start_days)
 
-    if contour_plot == 1:
+    # If shifted is 1 then the epochs already have the minima aligned with day 0
+    elif shifted == 1:
+        for j in range(NUM_EVENTS):
+            temp = np.where(dates == epoch_starts[j])
+            temp = int(temp[0][0])
+            for k in range(EPOCH_LENGTH):
+                gcrarr[k, j] = counts[temp + k]
+
+    gcr_mean = np.zeros(EPOCH_LENGTH)  # for each day average events together
+    gcr_std = np.zeros(EPOCH_LENGTH)
+    for j in range(EPOCH_LENGTH):
+        gcr_mean[j] = np.nanmean(gcrarr[j, :])
+        gcr_std[j] = np.nanstd(gcrarr[j, :])
+
+    # Makes a surface plot and line plot for all events. Also the mean plot
+    # with standard error
+    if plot == 1:
         fig1, ax1 = plt.subplots()
         sns.set_context("talk")
         cmap = ListedColormap(sns.color_palette("RdBu_r", 50))
-        fax = ax1.contourf(np.arange(-14, 36, 1), np.arange(1, len(start_days) + 1), gcrarr.transpose(),
+        fax = ax1.contourf(PLOT_RANGE, np.arange(1, len(epoch_starts) + 1), gcrarr.transpose(),
                            np.arange(-5, 5, 0.5), cmap=cmap, extend='both')
         plt.ylabel("Event Number")
         plt.xlabel("Day from Event")
@@ -105,124 +144,108 @@ def findmeangcr(start_days, contour_plot=0):
         cb.set_label("Moscow Neutron Count Anomaly")
         plt.show()
 
-    sns.set(context="talk", style="darkgrid")
-    sns.set_palette('hls', 12)
-    fig, ax = plt.subplots(figsize=(20, 10))
-    plt.plot(np.arange(-14, 36, 1), gcrarr)
-    plt.ylabel("Neutron Count Anomaly")
-    plt.xlabel("Day from Event")
-    plt.title("Forbush Decreases: 2002-15")
-    plt.show()
+        sns.set(context="talk", style="darkgrid")
+        sns.set_palette('hls', 12)
+        fig, ax = plt.subplots(figsize=(20, 10))
+        plt.plot(PLOT_RANGE, gcrarr)
+        plt.ylabel("Neutron Count Anomaly")
+        plt.xlabel("Day from Event")
+        plt.title("Forbush Decreases: 2002-15")
+        plt.show()
 
-    gcr_mean = np.zeros(EPOCH_LENGTH)  # for each day average events together
-    gcr_std = np.zeros(EPOCH_LENGTH)
-    for j in range(EPOCH_LENGTH):
-        gcr_mean[j] = np.nanmean(gcrarr[j, :])
-        gcr_std[j] = np.nanstd(gcrarr[j, :])
+        sns.set(context="talk", style="darkgrid")
+        sns.set_palette('hls', 12)
+        plt.plot(PLOT_RANGE, gcr_mean, 'b')
+        plt.plot(PLOT_RANGE, gcr_mean + (gcr_std / np.sqrt(NUM_EVENTS)), 'm')
+        plt.plot(PLOT_RANGE, gcr_mean - (gcr_std / np.sqrt(NUM_EVENTS)), 'm')
+        plt.ylabel("Mean Neutron Count Anomaly")
+        plt.xlabel("Day from Event")
+        plt.title("Forbush Decreases: 2002-15")
+        plt.show()
 
-    return gcr_mean, gcr_std, new_start_days
-
-
-# -----------------------------------------------------------------------------
-def standardize(xarr):
-    return (xarr - xarr.mean()) / xarr.std()
-
-
-# -----------------------------------------------------------------------------
-def longtermanomaly(xarr):
-    return (xarr - xarr.mean()) / xarr.mean()
-
-
-# -----------------------------------------------------------------------------
-def monthlyanomaly(xarr):
-    """
-    Anomaly as difference from monthly mean. Behaves strangely... value is zero
-    at end of every year...
-    :param xarr: xarray (reduced to 1 variable in time dimension)
-    :return: monthly anomaly of xarr
-    """
-    # TODO: figure out why this function has weird results
-    month_arr = np.array(xarr.time.dt.month)  # months corresponding to each daily value
-    monthlymeans = xarr.groupby('time.month').mean()  # take mean of each month
-    # For each day find the variation from the mean for that month
-    for j in range(12):
-        c = np.where(month_arr == j)
-        xarr[c] = (xarr[c] - monthlymeans[j]) / monthlymeans[j]
-    return xarr
+    return gcr_mean
 
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
+    # Number of days in each epoch. 14 days (2 weeks) before event to 35 days (5 weeks) after event.
+    EPOCH_LENGTH = 50
+    PLOT_RANGE = np.arange(-14, 36, 1)
+    EVENTS = ['2002-02-04', '2002-03-18', '2002-04-17', '2002-05-23', '2002-08-18', '2003-01-26', '2003-05-29',
+              '2003-08-17', '2003-10-30', '2003-11-30', '2004-07-26', '2004-09-13', '2004-11-09', '2005-01-18',
+              '2005-05-15', '2005-07-17', '2005-09-11', '2006-07-09', '2006-12-14', '2007-01-29', '2010-08-03',
+              '2011-04-11', '2011-06-22', '2011-08-05', '2011-10-30', '2012-01-22', '2012-03-08', '2012-04-04',
+              '2012-07-14', '2012-09-04', '2012-10-11', '2012-11-23', '2013-01-16', '2013-03-17', '2013-05-31',
+              '2013-07-12', '2014-04-18', '2014-06-17', '2014-09-12', '2014-12-21', '2015-03-17', '2015-05-06',
+              '2015-06-22', '2015-09-07', '2015-11-06']
+    NUM_EVENTS = len(EVENTS)
 
-    start_dates = []
-    end_dates = []
-    for i in EVENTS:
-        # look 2 weeks before and 6 weeks after (effects could take up to a month...)
-        s = datetime.datetime.strptime(i, "%Y-%m-%d") + datetime.timedelta(-14)
-        e = datetime.datetime.strptime(i, "%Y-%m-%d") + datetime.timedelta(42)
-        s = s.strftime('%Y-%m-%d')
-        start_dates.append(s)
-        e = e.strftime('%Y-%m-%d')
-        end_dates.append(e)
+    try:  # check if a file containing shifted epoch start dates exists. If so use that.
+        start_dates = np.load('shifted_epoch_onsets.npy')
+        end_dates = []
+        for i in start_dates:
+            # look 2 weeks before and 5 weeks after (epoch of 50 days).
+            e = datetime.datetime.strptime(i, "%Y-%m-%d") + datetime.timedelta(49)
+            e = e.strftime('%Y-%m-%d')
+            end_dates.append(e)
 
-    # Standard error = std / sqrt(n)
-    meangcr, stdgcr, new_start_dates = findmeangcr(start_dates, contour_plot=1)
-    sns.set(context="talk", style="darkgrid")
-    sns.set_palette('hls', 12)
-    plt.plot(np.arange(-14, 36, 1), meangcr)
-    plt.plot(np.arange(-14, 36, 1), meangcr + (stdgcr / np.sqrt(NUM_EVENTS)))
-    plt.plot(np.arange(-14, 36, 1), meangcr - (stdgcr / np.sqrt(NUM_EVENTS)))
-    plt.ylabel("Mean Neutron Count Anomaly")
-    plt.xlabel("Day from Event")
-    plt.title("Forbush Decreases: 2002-15")
-    plt.show()
+        mean_gcr = find_mean_gcr(start_dates, plot=0, shifted=1)
 
-    """
-    altofinterest = 20.5
+    except:  # if epoch start file doesn't exist use initial IZMIRAN events to create it.
+        print("Using non-shifted event onsets")
+        start_dates = []
+        end_dates = []
+        for i in EVENTS:
+            # look 2 weeks before and 5 weeks after (effects could take up to a month...)
+            s = datetime.datetime.strptime(i, "%Y-%m-%d") + datetime.timedelta(-14)
+            e = datetime.datetime.strptime(i, "%Y-%m-%d") + datetime.timedelta(35)
+            s = s.strftime('%Y-%m-%d')
+            start_dates.append(s)
+            e = e.strftime('%Y-%m-%d')
+            end_dates.append(e)
+
+        mean_gcr = find_mean_gcr(start_dates, plot=0, shifted=0)
+
+    alt_of_interest = 31.5
     # Data only exists up to 2012 for version 6
-    # x = osiris_nc.aer_level2_from_nc(nc_folder="/home/kimberlee/OsirisData/Level2/daily_version600/",
-    #                                 version='6.00', start_date='2002-01-01', end_date='2013-12-31')
-    x = omps_2d.aer_level2_from_nc('/home/kimberlee/ValhallaData/OMPS/OMPS_Tomography_v1.0.2/L2')
+    x = osiris_nc.aer_level2_from_nc(nc_folder="/home/kimberlee/OsirisData/Level2/daily/",
+                                     version='5.07', start_date='2002-01-01', end_date='2016-12-31')
+    # x = omps_2d.aer_level2_from_nc('/home/kimberlee/ValhallaData/OMPS/OMPS_Tomography_v1.0.2/L2')
 
-    x.to_netcdf(path='/home/kimberlee/Masters/OMPS_all', mode='w')
+    # x.to_netcdf(path='/home/kimberlee/Masters/OMPS_all', mode='w')
     # x = xarray.open_dataset('/home/kimberlee/Masters/test_o3658')
-    print(x)
-    exit()
+    # print(x)
+    # exit()
 
     a = x.where(x.latitude < 20)  # select location
     b = a.where(a.latitude > -20)
-    km20 = b.aerosol.sel(altitude=altofinterest)  # Choose either aerosol or angstrom here
+    km20 = b.aerosol.sel(altitude=alt_of_interest)  # Choose either aerosol or angstrom here
     km20_daily = km20.resample('D', dim='time', how='mean')  # take daily averages
-    # km20_daily = longtermanomaly(km20_daily)  # find anomaly
 
-    numevents = len(events)
-    eventarr = np.zeros((50, numevents))  # days, num. events
+    # km20_daily = anomalize(km20_daily)  # find normalized anomaly
+    km20_daily = 100 * ((km20_daily - km20_daily.mean()) / km20_daily.mean())
+    km20_rolling = km20_daily.rolling(time=35, min_periods=5).mean()
+    km20_daily = km20_daily - km20_rolling
 
-    means = np.zeros(numevents)
-    for i in range(numevents):
-        print(start_dates[i])
-        eventarr[:, i] = km20_daily.sel(time=slice(start_dates[i], end_dates[i]))
-        means[i] = np.nanmean(eventarr[:, i])
+    eventarr = np.zeros((EPOCH_LENGTH, NUM_EVENTS))  # days, num. events
+    for i in range(NUM_EVENTS):
+        try:
+            eventarr[:, i] = km20_daily.sel(time=slice(start_dates[i], end_dates[i]))
+        except:
+            eventarr[:, i] = np.nan
+            print("No data to load: %s" % start_dates[i])
 
-    print(means)
-
-    grand_mean = np.nanmean(means)
-    print(grand_mean)
-    eventarr = eventarr - grand_mean
-
-    compm = np.zeros(50)  # average events together for each day
-    for i in range(50):
+    compm = np.zeros(EPOCH_LENGTH)  # average events together for each day
+    for i in range(EPOCH_LENGTH):
         compm[i] = np.nanmean(eventarr[i, :])
 
     sns.set(context="talk", style="darkgrid")
     sns.set_palette('hls', 12)
-    fig, ax = plt.subplots(figsize=(10, 12))
-    plt.plot(np.arange(-14, 36, 1), eventarr)
-    plt.plot(np.arange(-14, 36, 1), compm, 'k')
-    plt.ylabel("Aerosol Extinction Anomaly")
+    fig, ax = plt.subplots(figsize=(20, 8))
+    plt.plot(PLOT_RANGE, eventarr)
+    plt.plot(PLOT_RANGE, compm, 'k')
+    plt.ylabel("OSIRIS Aerosol Extinction Anomaly - 5.07")
     plt.xlabel("Day from Event")
-    plt.title("Forbush Decreases: 2002-11")
+    plt.title("2002-15, %i km, tropics" % alt_of_interest)
     plt.show()
-
-    """
 
