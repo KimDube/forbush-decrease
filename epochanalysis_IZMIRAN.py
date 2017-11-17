fc3d034e483e97d64b409918ac5fc97279ff1cf1
Kimberlee <krd020@mail.usask.ca>
2017-11-17 09:49:31 -0600

    Significance testing is working and applied to epoch analysis

--- epochanalysis_IZMIRAN.py
+++ epochanalysis_IZMIRAN.py
@@ -123,13 +123,16 @@ def find_mean_gcr(epoch_starts, plot=0, shifted=0):
             for k in range(EPOCH_LENGTH):
                 gcrarr[k, j] = counts[temp + k]
 
+            # sns.set(context="talk", style="darkgrid")
+            # sns.set_palette('hls', 12)
+            # plt.plot(PLOT_RANGE, gcrarr[:, j])
+            # plt.show()
+
     gcr_mean = np.zeros(EPOCH_LENGTH)  # for each day average events together
     gcr_std = np.zeros(EPOCH_LENGTH)
     for j in range(EPOCH_LENGTH):
         gcr_mean[j] = np.nanmean(gcrarr[j, :])
         gcr_std[j] = np.nanstd(gcrarr[j, :])
-
-    gcr_mean_err, gcr_std_err = mc.significance_test(counts, EPOCH_LENGTH, NUM_EVENTS, 10000)
 
     # Makes a surface plot and line plot for all events. Also the mean plot
     # with standard error
@@ -146,6 +149,8 @@ def find_mean_gcr(epoch_starts, plot=0, shifted=0):
         plt.gca().invert_yaxis()
         cb = plt.colorbar(fax, orientation='horizontal', fraction=0.2, aspect=30)
         cb.set_label("Moscow Neutron Count Anomaly")
+        plt.tight_layout()
+        plt.savefig('/home/kimberlee/Masters/ForbushDecrease/plots/MOSC_surf.png', dpi=300)
         plt.show()
 
         sns.set(context="talk", style="darkgrid")
@@ -158,11 +163,13 @@ def find_mean_gcr(epoch_starts, plot=0, shifted=0):
         plt.show()
         """
 
+        gcr_mean_err, gcr_std_err = mc.significance_test(counts, EPOCH_LENGTH, NUM_EVENTS, 10000)
+
         sns.set(context="talk", style="darkgrid")
-        sns.set_palette('hls', 12)
-        plt.plot(PLOT_RANGE, gcr_mean, 'b')
-        plt.plot(PLOT_RANGE, gcr_mean + (gcr_std / np.sqrt(NUM_EVENTS)), 'm')
-        plt.plot(PLOT_RANGE, gcr_mean - (gcr_std / np.sqrt(NUM_EVENTS)), 'm')
+        sns.set_color_codes("dark")
+        plt.plot(PLOT_RANGE, gcr_mean, 'r')
+        plt.plot(PLOT_RANGE, gcr_mean + (gcr_std / np.sqrt(NUM_EVENTS)), 'b', linewidth=0.5)
+        plt.plot(PLOT_RANGE, gcr_mean - (gcr_std / np.sqrt(NUM_EVENTS)), 'b', linewidth=0.5)
 
         plt.plot(PLOT_RANGE, gcr_mean_err + 1.96 * gcr_std_err, 'k-.')
         plt.plot(PLOT_RANGE, gcr_mean_err - 1.96 * gcr_std_err, 'k-.')
@@ -170,6 +177,7 @@ def find_mean_gcr(epoch_starts, plot=0, shifted=0):
         plt.ylabel("Mean Neutron Count Anomaly")
         plt.xlabel("Day from Event")
         plt.title("Forbush Decreases: 2002-15")
+        plt.savefig('/home/kimberlee/Masters/ForbushDecrease/plots/MOSC_mean_signif.png', dpi=300)
         plt.show()
 
     return gcr_mean
@@ -181,9 +189,9 @@ if __name__ == "__main__":
     EPOCH_LENGTH = 50
     PLOT_RANGE = np.arange(-14, 36, 1)
     EVENTS = ['2002-02-04', '2002-03-18', '2002-04-17', '2002-05-23', '2002-08-18', '2003-01-26', '2003-05-29',
-              '2003-08-17', '2003-10-30', '2003-11-30', '2004-07-26', '2004-09-13', '2004-11-09', '2005-01-18',
+              '2003-08-17', '2003-10-30', '2003-11-20', '2004-07-26', '2004-09-13', '2004-11-09', '2005-01-18',
               '2005-05-15', '2005-07-17', '2005-09-11', '2006-07-09', '2006-12-14', '2007-01-29', '2010-08-03',
-              '2011-04-11', '2011-06-22', '2011-08-05', '2011-10-30', '2012-01-22', '2012-03-08', '2012-04-04',
+              '2011-04-11', '2011-06-22', '2011-08-05', '2011-10-24', '2012-01-22', '2012-03-08', '2012-04-04',
               '2012-07-14', '2012-09-04', '2012-10-11', '2012-11-23', '2013-01-16', '2013-03-17', '2013-05-31',
               '2013-07-12', '2014-04-18', '2014-06-17', '2014-09-12', '2014-12-21', '2015-03-17', '2015-05-06',
               '2015-06-22', '2015-09-07', '2015-11-06']
@@ -215,19 +223,23 @@ if __name__ == "__main__":
 
         mean_gcr = find_mean_gcr(start_dates, plot=0, shifted=0)
 
-    alt_of_interest = 20.5
+    alt_of_interest = 30.5
+
     # Data only exists up to 2012 for version 6
-    x = osiris_nc.aer_level2_from_nc(nc_folder="/home/kimberlee/OsirisData/Level2/daily/",
-                                     version='5.07', start_date='2002-01-01', end_date='2016-12-31')
-    # x = omps_2d.aer_level2_from_nc('/home/kimberlee/ValhallaData/OMPS/OMPS_Tomography_v1.0.2/L2')
+    # x = osiris_nc.aer_level2_from_nc(nc_folder="/home/kimberlee/OsirisData/Level2/daily/",
+    #                                 version='5.07', start_date='2002-01-01', end_date='2016-12-31')
+    # x = osiris_nc.aer_level2_from_nc(nc_folder="/home/kimberlee/OsirisData/Level2/daily_version600/",
+    #                                 version='6.00', start_date='2002-01-01', end_date='2016-12-31')
 
     # x.to_netcdf(path='/home/kimberlee/Masters/OMPS_all', mode='w')
-    # x = xarray.open_dataset('/home/kimberlee/Masters/test_o3658')
-    # print(x)
     # exit()
+    # x = xarray.open_dataset('/home/kimberlee/Masters/OMPS_all')
 
-    a = x.where(x.latitude < 20)  # select location
-    b = a.where(a.latitude > -20)
+    # OMPS: computer can't handle saving
+    x = omps_2d.aer_level2_from_nc('/home/kimberlee/ValhallaData/OMPS/OMPS_Tomography_v1.0.2/L2')
+
+    # a = x.where(x.latitude < 20)  # select location
+    b = x  # a.where(a.latitude > -20)
     km20 = b.aerosol.sel(altitude=alt_of_interest)  # Choose either aerosol or angstrom here
     km20_daily = km20.resample('D', dim='time', how='mean')  # take daily averages
 
@@ -237,30 +249,37 @@ if __name__ == "__main__":
     km20_daily = km20_daily - km20_rolling
 
     eventarr = np.zeros((EPOCH_LENGTH, NUM_EVENTS))  # days, num. events
+    events_with_data = 0
     for i in range(NUM_EVENTS):
         try:
             eventarr[:, i] = km20_daily.sel(time=slice(start_dates[i], end_dates[i]))
+            events_with_data += 1
         except:
             eventarr[:, i] = np.nan
             print("No data to load: %s" % start_dates[i])
 
     compm = np.zeros(EPOCH_LENGTH)  # average events together for each day
+    comps = np.zeros(EPOCH_LENGTH)
     for i in range(EPOCH_LENGTH):
-        compm[i] = np.nanmean(eventarr[i, :])
+        compm[i] = np.nanmedian(eventarr[i, :])
+        comps[i] = np.nanstd(eventarr[i, :])
 
+    NUM_EVENTS = events_with_data
     aer_mean_err, aer_std_err = mc.significance_test(km20_daily, EPOCH_LENGTH, NUM_EVENTS, 10000)
 
     sns.set(context="talk", style="darkgrid")
-    sns.set_palette('hls', 12)
+    sns.set_color_codes("dark")
     fig, ax = plt.subplots(figsize=(20, 8))
     # plt.plot(PLOT_RANGE, eventarr)
-    plt.plot(PLOT_RANGE, compm)
+    plt.plot(PLOT_RANGE, compm, 'r')
     plt.plot(PLOT_RANGE, aer_mean_err + 1.96 * aer_std_err, 'k-.')
     plt.plot(PLOT_RANGE, aer_mean_err - 1.96 * aer_std_err, 'k-.')
+    plt.plot(PLOT_RANGE, compm + (comps / np.sqrt(NUM_EVENTS)), 'b', linewidth=0.5)
+    plt.plot(PLOT_RANGE, compm - (comps / np.sqrt(NUM_EVENTS)), 'b', linewidth=0.5)
 
-    plt.ylabel("OSIRIS Aerosol Extinction Anomaly - 5.07")
+    plt.ylabel("OMPS Aerosol Extinction Anomaly")
     plt.xlabel("Day from Event")
     plt.title("2002-15, %i km, tropics" % alt_of_interest)
-    plt.savefig('/home/kimberlee/Masters/ForbushDecrease/OSIRIS_line_507_%ikm' % alt_of_interest, dpi=300)
+    plt.savefig('/home/kimberlee/Masters/ForbushDecrease/OMPS_%ikm' % alt_of_interest, dpi=300)
     plt.show()
 
